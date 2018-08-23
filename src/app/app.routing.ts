705b6c356b592fda2d0ca7f12c8916925800e0d9
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-08-23 11:37:34 -0300

    produto

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AdministrativoComponent } from './sistema/administrativo/administrativo.component';
 import { UsuarioComponent } from './sistema/administrativo/usuario/usuario.component';
+import { ProdutosComponent } from './sistema/administrativo/produtos/produtos.component';
+
 
 
 const APP_ROUTES: Routes = [
     { path: '', component: AdministrativoComponent },
-    { path: 'sistema/administrativo/usuario', component: UsuarioComponent }
+    { path: 'sistema/administrativo/usuario', component: UsuarioComponent },
+    { path: 'sistema/administrativo/produtos', component:  ProdutosComponent }
    // { path: 'sistema/administrativo/inicio', component: AdministrativoComponent }
 ];
 
